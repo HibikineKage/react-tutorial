11443a3f949c43b19b9a749b0fc354e5b638a187
Hibikine Kage <goodsmile314159@gmail.com>
2017-06-28 12:48:11 +0900

    不要な記述を削除

--- src/board.js
+++ src/board.js
@@ -19,14 +19,6 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
         <div className="board-row">
